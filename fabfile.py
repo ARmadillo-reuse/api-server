079ace86343c3e0188376ae9d9b576191ecab829
Alex Konradi <akonradi@mit.edu>
2014-03-02 02:42:53 -0500

    Fix fabfile.py to work with virtualenv

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,12 @@
 from __future__ import with_statement
-from fabric.api import env, settings, local, run, cd, abort
+from fabric.api import env, settings, local, run, cd, abort, prefix
 from fabric.contrib.console import confirm
 
-env.hosts = ['armadillo.xvm.mit.edu']
+env.hosts = ['armadillo@armadillo.xvm.mit.edu']
 
 def test():
     with settings(warn_only=False):
-        result = local('"./manage.py" test' )
+        result = local('"./manage.py" test')
     if result.failed and not confirm("Tests failed. Continue anyway?"):
         abort("Aborting at user request.")
 
@@ -23,13 +23,13 @@ def prepare_deploy():
 
 def deploy(app="web_api", stable=False):
     branch = "master" if stable else "dev"
-    code_dir = "/home/armadillo/server-%s"%("stable" if stable else "unstable")
-    with settings(warn_only=True):
-        if run("test -d %s" % code_dir).failed:
-            git_origin = "https://github.com/ARmadillo-reuse/api-server.git"
-            run("git clone %s %s" % (git_origin, code_dir))
-            
-    with cd(code_dir):
-        run("git pull origin %s" % branch)
-        local("python manage.py migrate %s" % app)
-        local("python manage.py test")
+    environment = "server-" + ("stable" if stable else "unstable")
+    code_dir = "/home/armadillo/%s" % environment
+
+    with prefix('WORKON_HOME=$HOME/.virtualenvs'):
+        with prefix('source /usr/local/bin/virtualenvwrapper.sh'):
+            with prefix('workon %s' % environment):
+                with cd(code_dir):
+                    run("git pull origin %s" % branch)
+                    run("python manage.py migrate %s" % app)
+                    run("python manage.py test")
