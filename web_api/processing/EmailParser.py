dfa058add3c0a59f4ffc8ae669579f7f0a634d52
Alex Konradi <akonradi@mit.edu>
2014-03-30 21:49:40 -0400

    Add thread detection to EmailParser
    It currently uses just the subject, and returns an EmailThread object if
    one is found, or None otherwise.

--- web_api/processing/EmailParser.py
+++ web_api/processing/EmailParser.py
@@ -4,11 +4,13 @@ Created on Mar 28, 2014
 @author: Alex Konradi
 '''
 from collections import Counter
+from datetime import timedelta
 from math import sqrt
 import operator
 import re
 
 from django.db.models import Q
+import django.utils.timezone
 
 from web_api.models import EmailThread
 
@@ -21,13 +23,21 @@ class EmailParser(object):
     web_api.models
     '''
 
-    def __init__(self):
+    def __init__(self, thread_death_timeout=timedelta(7)):
+        """
+        Construct an EmailParser object with the specified timeout for a thread
+        
+        @param thread_death_timeout: the amount of time after which a thread is
+               considered closed
+        """
+        self.thread_death_timeout = thread_death_timeout
         pass
     
     def parse_email_type(self, email):
         """
         Determine the type of object for the given email key-value dictionary.
         """
+        pass
         
     
     def parse_email_thread(self, email):
@@ -35,9 +45,12 @@ class EmailParser(object):
                          email["subject"], flags=re.IGNORECASE).strip()
 
         subject_words = subject.split()
-        query = reduce(lambda x,y: x | y, (Q(subject__contains=word)
+        query = reduce(lambda x, y: x | y, (Q(subject__contains=word)
                        for word in set(subject_words)))
-        possible_threads = EmailThread.objects.filter(query)
+        time_filter = Q(modified__gt=django.utils.timezone.now()
+                        - self.thread_death_timeout)
+        
+        possible_threads = EmailThread.objects.filter(query & time_filter)
         
         chosen_thread = (0, None)
         for thread in possible_threads:           
@@ -56,12 +69,11 @@ class EmailParser(object):
     
     def dot_dist(self, words1, words2):
         w1, w2 = [Counter(words1), Counter(words2)]
-        num = sum(w1[word]*w2[word] for word in w1)
+        num = sum(w1[word] * w2[word] for word in w1)
         
         denom = sqrt(reduce(operator.mul,
-                       (sum(x**2 for x in w.values())
+                       (sum(x ** 2 for x in w.values())
                        for w in [w1, w2])))
         
-        return float(num)/denom
-    
+        return float(num) / denom
     
