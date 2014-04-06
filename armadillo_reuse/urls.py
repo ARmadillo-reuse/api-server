3a6d9b72da2b1937485de150f2fd13b6bb9bb8c9
Shalom <shaladi@mit.edu>
2014-04-06 17:08:57 -0400

    activated admin page

--- armadillo_reuse/urls.py
+++ armadillo_reuse/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-#from django.contrib import admin
-#admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 
     #direct all api calls to the web_api application
     url(r'^api/', include('web_api.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
