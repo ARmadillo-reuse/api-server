79c8b306c7b279a7c91629157c7789da84d3ade7
Shalom <shaladi@mit.edu>
2014-04-25 12:44:28 -0400

    added all models to admin page

--- web_api/admin.py
+++ web_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from web_api.models import Item, UserLogEvent, EmailThread, NewPostEmail, ClaimedItemEmail, GcmUser
 # Register your models here.
+admin.site.register(Item)
+admin.site.register(UserLogEvent)
+admin.site.register(EmailThread)
+admin.site.register(NewPostEmail)
+admin.site.register(ClaimedItemEmail)
+admin.site.register(GcmUser)
